31c4a1b61656bf11a6beed26360fda1b2725c456
GroverTB <grover.jtb@gmail.com>
2020-01-13 09:36:24 -0500

    [ UPDATE ] pages

---
+++ docs/src/pages/views/dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MarkdownDocs from 'components/MarkDowns/MarkdownDocs';
+
+const req = require.context('containers/views/dashboard', false, /\.(md|js|tsx)$/);
+const reqSource = require.context(
+  '!raw-loader!containers/views/dashboard',
+  false,
+  /\.(js|tsx)$/,
+);
+const reqPrefix = 'containers/views/dashboard';
+
+function Page() {
+  return <MarkdownDocs disableAd req={req} reqSource={reqSource} reqPrefix={reqPrefix} />;
+}
+
+export default Page;
--- docs/src/routes/pages.tsx
+++ docs/src/routes/pages.tsx
@@ -132,6 +132,10 @@ const pages: Array<Page> = [
         title: 'Card User',
         path: '/views/carduser'
       },
+      {
+        title: 'Dashboard',
+        path: '/views/dashboard'
+      },
     ]
   }
   // {
