669a59269513b116931af170f2998aef7822e728
Caro <dica.alderete@gmail.com>
2020-01-14 17:33:16 -0500

    [UPDATE]

--- packages/krowdy-ui-views/src/index.js
+++ packages/krowdy-ui-views/src/index.js
@@ -1,11 +1,7 @@
 export { default as Header } from './Header'
 export { default as JobDetail } from './JobDetail'
-<<<<<<< HEAD
-export { default as CardUser } from './CardUser'
 export { default as SelectCandidates } from './SelectCandidates'
 export { default as CardCandidate } from './CardCandidate'
 export { default as Search } from './Search'
-=======
 export { default as Questionary } from './Questionary'
 export * from './Cards'
->>>>>>> a0d4498ef6262c8c16a8d7f9576c57d2d086b0f0
