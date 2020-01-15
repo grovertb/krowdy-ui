7ce29305640d6186b35dfc6cb2da6e1c3caaa207
Caro <dica.alderete@gmail.com>
2020-01-15 12:30:56 -0500

    [UPDATE] add styles to header task

--- docs/src/containers/views/headertask/HeaderTask.js
+++ docs/src/containers/views/headertask/HeaderTask.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import { HeaderTask } from '@krowdy-ui/views'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
 
 export default function () {
+  const nameTask = 'Video Cuestionario'
+  const arrowBackIcon = <ArrowBackIcon />
+  const titleTask = 'Titulo de video entrevista'
+  const numberCandidates = 10
+  const index = 0
+  const id = null
 
   return (
     <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', margin: '50px' }}>
-      <HeaderTask />
+      <HeaderTask
+        // onClickSelectCandidates={}
+        // onClickUpdateTask={}
+        // onClickSave={}
+        // onClickActiveTask={}
+        // onChangeTitleTask={}
+        // onKeyUpTitleTask={}
+        // valueTitleTask={}
+        iconArrow={arrowBackIcon}
+        nameTask={nameTask}
+        titleTask={titleTask}
+        numberCandidates={numberCandidates}
+        index={index}
+        id={id}
+      />
     </div>
   )
 }
--- packages/krowdy-ui-views/src/HeaderTask/HeaderTask.js
+++ packages/krowdy-ui-views/src/HeaderTask/HeaderTask.js
@@ -1,76 +1,203 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@krowdy-ui/styles'
-import { Paper, Typography, Button, TextField, Switch, IconButton } from '@krowdy-ui/core'
-import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import {
+	Paper,
+	Typography,
+	Button,
+	TextField,
+	Switch,
+	IconButton,
+	FormControlLabel
+} from '@krowdy-ui/core'
+
 
 export const styles = theme => ({
-  headerLeft: {
-    flex: '1'
-  },
-  iconArrow: {
-    height: 18,
-    width: 18
-  },
-  paper: {
-    display: 'flex',
-    height: 56,
-    width: 1308
-  }
+	buttonsRight: {
+		alignItems: 'center',
+		display: 'flex'
+	},
+	headerLeft: {
+		flex: '1'
+	},
+	iconArrow: {
+		height: 18,
+		width: 18
+	},
+	labelSwitch: {
+		fontSize: '14px !important',
+		fontStyle: 'normal',
+		fontWeight: 'normal',
+		lineHeight: '20px',
+	},
+	nameTask: {
+		fontSize: 18,
+		fontStyle: 'normal',
+		fontWeight: 'bold',
+		lineHeight: '24px',
+		marginRight: 12,
+	},
+	paper: {
+		alignItems: 'center',
+		display: 'flex',
+		height: 56,
+		justifyContent: 'space-between',
+		width: 1308
+	},
+	'switch': {
+		marginRight: 6
+	},
+	task: {
+		alignItems: 'center',
+		display: 'flex'
+	},
+	textButton: {
+		fontSize: 12,
+		fontStyle: 'normal',
+		fontWeight: 'normal',
+		lineHeight: '100 %',
+		marginRight: 12,
+		textAlign: 'center'
+	},
+	textTextfield: {
+		fontSize: 14,
+		fontStyle: 'normal',
+		fontWeight: 'normal',
+		lineHeight: '100%',
+	},
+	titleTask: {
+		marginBottom: 10
+	}
+
 })
 
 const HeaderTask = props => {
-  const activeTask = false
-  const {
-    classes,
+	// const nameTask = 'Video Cuestionario'
+	// const titleTask = 'Titulo de video entrevista'
+	// const numberCandidates = 10
+	// const index = 0
+	// const id = null
+	const {
+		classes,
+		onClickSelectCandidates,
+		onClickUpdateTask,
+		onClickSave,
+		onClickActiveTask,
+		onChangeTitleTask,
+		onKeyUpTitleTask,
+		nameTask,
+		titleTask,
+		valueTitleTask,
+		numberCandidates,
+		index,
+		iconArrow,
+		id
+	} = props
 
-  } = props
-
-  return (
-    <Paper className={classes.paper}>
-      <IconButton className={classes.iconArrow}>
-        <ArrowBackIcon />
-      </IconButton>
-      <Typography>
-        Video Cuestionario
-      </Typography>
-      <Button>
-        10 Candidatos - Etapa 1
-      </Button>
-
-      <TextField
-        label='Titulo de video entrevista'
-      >
-
-      </TextField>
-      {
-        activeTask
-          ?
-          <div>
-            <Switch>
-
-            </Switch>
-            <Button>
-              Actualizar tarea</Button>
-          </div>
-          :
-          <div>
-            <Button>
-              Guardar Borrador
+	return (
+		<Paper className={classes.paper}>
+			<div className={classes.task}>
+				<IconButton >
+					{iconArrow}
+				</IconButton>
+				<Typography className={classes.nameTask}>
+					{nameTask}
+				</Typography>
+				<Button
+					variant='outlined'
+					color='primary'
+					onClick={onClickSelectCandidates}
+					className={classes.textButton}>
+					{`${numberCandidates} Candidatos - Etapa ${index + 1}`}
+				</Button>
+				<TextField
+					onChange={onChangeTitleTask}
+					onKeyUp={onKeyUpTitleTask}
+					value={valueTitleTask}
+					className={classes.titleTask}
+					label={titleTask}
+					InputLabelProps={{
+						classes: {
+							root: classes.textTextfield,
+						}
+					}}
+					InputProps={{
+						classes: {
+							root: classes.textTextfield
+						}
+					}}
+				>
+				</TextField>
+			</div>
+			<div className={classes.buttonsRight}>
+				{
+					id
+						?
+						<div>
+							<FormControlLabel
+								classes={{
+									label: classes.labelSwitch,
+								}}
+								className={classes.switch}
+								control={
+									<Switch
+										color='primary'
+										size='small'
+									// checked={checked} 
+									// onChange={toggleChecked}
+									/>
+								}
+								label='Tarea activada'
+								labelPlacement='start'
+							/>
+							<Button
+								onClick={onClickUpdateTask}
+								variant='outlined'
+								color='primary'
+								className={classes.textButton} >
+								Actualizar tarea
+                </Button>
+						</div>
+						:
+						<div>
+							<Button
+								onClick={onClickSave}
+								variant='outlined'
+								color='primary'
+								className={classes.textButton}>
+								Guardar Borrador
+              </Button>
+							<Button
+								onClick={onClickActiveTask}
+								variant='contained'
+								color='primary'
+								className={classes.textButton}>
+								Activar tarea
             </Button>
-            <Button>
-              Activar tarea
-            </Button>
-          </div>
+						</div>
 
-      }
-    </Paper>
+				}
+			</div>
 
-  )
+		</Paper >
+
+	)
 }
 
 HeaderTask.propTypes = {
-  classes: PropTypes.object,
+	classes: PropTypes.object,
+	id: PropTypes.number,
+	index: PropTypes.number,
+	nameTask: PropTypes.string,
+	numberCandidates: PropTypes.number,
+	onChangeTitleTask: PropTypes.any,
+	onClickActiveTask: PropTypes.any,
+	onClickSave: PropTypes.any,
+	onClickSelectCandidates: PropTypes.any,
+	onClickUpdateTask: PropTypes.any,
+	onKeyUpTitleTask: PropTypes.any,
+	titleTask: PropTypes.string,
+	valueTitleTask: PropTypes.any,
 
 }
 
