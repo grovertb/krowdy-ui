1d7d6cfb82cee382a6e2da5b15ef7fc79067d458
~xavi~ <xavivzla.g@gmail.com>
2020-01-24 16:29:59 -0500

    [UPDATE] - fix wrapper cotent view heigh

--- packages/krowdy-ui-views/src/Dashboard/Dashboard.js
+++ packages/krowdy-ui-views/src/Dashboard/Dashboard.js
@@ -244,8 +244,10 @@ const useStyles = makeStyles(theme => ({
     borderRadius  : 4,
     display       : 'flex',
     flex          : 1,
+    height        : 'calc(100vh - 88px)',
     justifyContent: 'center',
-    margin        : theme.spacing(1)
+    margin        : theme.spacing(1),
+    overflow      : 'auto'
   }
 }))
 
