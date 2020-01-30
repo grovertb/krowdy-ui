657343629e7ae88cb12a503ce8cf211d84900630
Caro <dica.alderete@gmail.com>
2020-01-30 12:03:29 -0500

    [UPDATE] add styles in pagination

--- packages/krowdy-ui-views/src/Pagination/Pagination.js
+++ packages/krowdy-ui-views/src/Pagination/Pagination.js
@@ -2,28 +2,75 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@krowdy-ui/styles'
 import {
-  Search as SearchIcon
+  KeyboardArrowLeft as ArrowLeftIcon,
+  KeyboardArrowRight as ArrowRightIcon
 } from '@material-ui/icons'
+import { Box, Typography, InputBase, Select, MenuItem, Input, IconButton } from '@krowdy-ui/core'
+import clsx from 'clsx'
+
 export const styles = () => ({
 
 })
 
 const Pagination = props => {
   const {
-    classes
+    classes,
+    onChangeLimitSelect,
+    valueLimitSelect,
+    backPage,
+    onChangeInput,
+    onKeyDownInput,
+    pageInput,
+    totalPages,
+    nextPage
   } = props
 
+  const limits = [ 20, 50, 100 ]
+
   return (
-    <div >
-      <SearchIcon />
+    <div  >
+      <Box alignItems='center' display='flex'>
+
+        <Typography>Mostrar</Typography>
+        <Select
+          input={<InputBase />}
+          onChange={onChangeLimitSelect}
+          value={valueLimitSelect}>
+          {limits.map((limit, index) => (
+            <MenuItem key={index} value={limit}>{limit}</MenuItem>
+          ))}
+        </Select>
+        <Box alignItems='center' display='flex'>
+          <IconButton>
+            <ArrowLeftIcon
+              className={classes.icon}
+              onClick={backPage} />
+          </IconButton>
+          <Input
+            classes={{
+              input: clsx(classes.input, classes.color),
+              root : classes.rootTextfield
+            }}
+            disableUnderline
+            onChange={onChangeInput}
+            onKeyDown={onKeyDownInput}
+            value={pageInput} />
+          <Typography className={classes.slash}>/</Typography>
+          <Typography>{totalPages > 0 ? totalPages : 1}</Typography>
+          <IconButton>
+            <ArrowRightIcon
+              className={classes.icon}
+              onClick={nextPage} />
+          </IconButton>
+        </Box>
+      </Box>
     </div >
 
   )
 }
 
 Pagination.propTypes = {
-  classes   : PropTypes.object,
-  searchIcon: PropTypes.node
+  classes: PropTypes.object
 }
 
 Pagination.muiName = 'Pagination'
