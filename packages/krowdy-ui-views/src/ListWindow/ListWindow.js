c9e9922f66a7262e5860f6171f1e6a78f023dd51
GroverTB <grover.jtb@gmail.com>
2020-01-30 15:53:56 -0500

    [ UPDATE ] eslint

--- packages/krowdy-ui-views/src/ListWindow/ListWindow.js
+++ packages/krowdy-ui-views/src/ListWindow/ListWindow.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React from 'react'
 import { FixedSizeList as List } from 'react-window'
 import InfiniteLoader from 'react-window-infinite-loader'
 
