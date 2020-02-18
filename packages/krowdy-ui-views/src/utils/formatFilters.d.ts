b5ed27c448c6bec0088121729b4717552e8ca427
GroverTB <grover.jtb@gmail.com>
2020-02-18 11:14:22 -0500

    [ UPDATE ] formatFilters

--- packages/krowdy-ui-views/src/utils/formatFilters.d.ts
+++ packages/krowdy-ui-views/src/utils/formatFilters.d.ts
@@ -2,8 +2,9 @@ type filterFront = {
   label?: String
   key?: String
   value?: []
+  filterType?: String
   operator?: String
   children?: filterFront[]
 }
 
-export default function formatFilters(filters: filterFront[]): filterFront[];
+export default function formatFilters(filters: filterFront[]): [];
