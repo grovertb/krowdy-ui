753a89c52a98d210d001b7925f4a132c316841b3
GroverTB <grover.jtb@gmail.com>
2020-02-19 16:39:18 -0500

    [ UPDATE ] formatFilters parse date

--- packages/krowdy-ui-views/src/utils/formatFilters.js
+++ packages/krowdy-ui-views/src/utils/formatFilters.js
@@ -1,14 +1,18 @@
+import XDate from 'xdate'
+
+const formatDate = date => new XDate(date).toString('yyyy/MM/dd') // 2020/01/20
+
 export default function formatFilters(filters) {
   return filters.map(({ key, operator, type, value, children = [] }) => {
     const child = children.length ? { $and: [ { $or: formatFilters(children) } ] } :{}
 
     const valueFilter = type === 'date' ?
       Array.isArray(value) ?
-        value.map(date => new Date(date)) : new Date(value) :
+        value.map(date => formatDate(date)) : formatDate(value) :
       value
 
     return {
-      [key]: (operator === '$range' && Array.isArray(valueFilter)) ?
+      [key]: (operator === '$range') ?
         {
           $gte: valueFilter[0],
           $lte: valueFilter[1],
