a0ec857f5209a4a181c8909980fad18120b8b752
GroverTB <grover.jtb@gmail.com>
2020-02-19 15:37:54 -0500

    [ UPDATE ] formatFilters

--- packages/krowdy-ui-views/src/utils/formatFilters.js
+++ packages/krowdy-ui-views/src/utils/formatFilters.js
@@ -1,12 +1,23 @@
 export default function formatFilters(filters) {
-  return filters.map(({ key, operator, filterType, value, children = [] }) => {
-    const params = children.length ? { $and: formatFilters(children) } :{}
+  return filters.map(({ key, operator, type, value, children = [] }) => {
+    const child = children.length ? { $and: formatFilters(children) } :{}
+
+    const valueFilter = type === 'date' ?
+      Array.isArray(value) ?
+        value.map(date => new Date(date)) : new Date(value) :
+      value
 
     return {
-      [key]: {
-        [operator]: filterType === 'date'? new Date(value) : value,
-        ...params
-      }
+      [key]: (operator === '$range' && Array.isArray(valueFilter)) ?
+        {
+          $gte: valueFilter[0],
+          $lte: valueFilter[1],
+          ...child
+        } :
+        {
+          [operator]: valueFilter,
+          ...child
+        }
     }
   })
 }
