feee0d453e53c3138a64fb2f484c32cb4b6b98d0
GroverTB <grover.jtb@gmail.com>
2020-02-21 15:04:43 -0500

    [ UPDATE ] formatFilters

--- packages/krowdy-ui-views/src/utils/formatFilters.js
+++ packages/krowdy-ui-views/src/utils/formatFilters.js
@@ -2,9 +2,27 @@ import XDate from 'xdate'
 
 const formatDate = date => new XDate(date).toString('yyyy/MM/dd') // 2020/01/20
 
+const formatRegex = value => {
+  const vocals = [
+    '[aáàãâä]',
+    '[eéèêë]',
+    '[iíìîï]',
+    '[oóòõôö]',
+    '[uúùûü]'
+  ]
+
+  let string = Array.isArray(value) ? value.join('|') : value
+
+  vocals.forEach(list => {
+    string = string.replace(new RegExp(list, 'gi'), list)
+  })
+
+  return new RegExp(`(^|)${string.replace(/ /g, '\\s*')}($|)\\b`, 'i')
+}
+
 export default function formatFilters(filters) {
   return filters.map(({ key, operator, type, value, children = [] }) => {
-    const child = children.length ? { $and: [ { $or: formatFilters(children) } ] } :{}
+    const child = children.length ? { $and: [ { $or: formatFilters(children) } ] } : {}
 
     const valueFilter = type === 'date' ?
       Array.isArray(value) ?
@@ -18,10 +36,14 @@ export default function formatFilters(filters) {
           $lte: valueFilter[1],
           ...child
         } :
-        {
-          [operator]: valueFilter,
+        (operator === '$regex' || operator === '$unregex') ? {
+          $regex: formatRegex(value),
           ...child
-        }
+        } :
+          {
+            [operator]: valueFilter,
+            ...child
+          }
     }
   })
 }
