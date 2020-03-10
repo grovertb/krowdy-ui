ad7bbf3b7a2fd6dc2850b579993d19c4c708ecd5
~xavi~ <xavivzla.g@gmail.com>
2020-03-10 16:30:53 -0500

    [UPDATE] - update dependencies and extends props

--- packages/krowdy-ui-views/src/TableGroup/TableGroup.js
+++ packages/krowdy-ui-views/src/TableGroup/TableGroup.js
@@ -2,20 +2,24 @@ import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@krowdy-ui/styles'
 import {
-  Grid,
   Paper,
   TextField,
-  InputAdornment
+  InputAdornment,
+  TablePagination
 } from '@krowdy-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
+import StarIcon from '@material-ui/icons/Star'
+import StarBorderIcon from '@material-ui/icons/StarBorder'
 import {
   GroupingState,
   SortingState,
+  IntegratedSorting,
   CustomGrouping
 } from '@devexpress/dx-react-grid'
 import {
   Grid as GridTable,
   Table,
+  // VirtualTable,
   TableHeaderRow,
   TableGroupRow,
   GroupingPanel,
@@ -39,6 +43,20 @@ export const styles = theme => ({
     display       : 'flex',
     justifyContent: 'flex-end',
     padding       : 20
+  },
+  gridTableChip: {
+    '&:focus': {
+      '& > div': {
+        '&:focus': {
+          background: '#F3FBFF'
+        }
+      }
+    },
+    '&:hover': {
+      background: '#F3FBFF'
+    },
+    background: '#F3FBFF',
+    border    : '1px solid #80DBFF'
   },
   gridTableGroupingEmpty: {
     '& > div': {
@@ -115,7 +133,15 @@ export const styles = theme => ({
   }
 })
 
-const formatColumns = columns => columns.map(column => ({ name: column.key, title: column.label }))
+const formatColumnsExtension = columns => columns.map(column => {
+  if(column.type)
+    if(column.type === 'star')
+      column.width = 160
+
+  return ({ ...column, columnName: column.key, title: column.label })
+})
+
+const formatColumns = columns => columns.map(column => ({ ...column, name: column.key, title: column.label }))
 
 const columnsDraggableDisable = columns => columns.map(column => ({ columnName: column.key, groupingEnabled: column.draggable }))
 
@@ -124,29 +150,77 @@ const sortingFormatReverse = sortings => sortings.map(sorting => ({ orderBy: sor
 
 const getRowId = row => row._id
 
+const TableRow = ({ row, ...restProps }, callback) => (
+  <Table.Row
+    {...restProps}
+    onClick={() => callback(row._id)}
+    style={{
+      cursor: 'pointer'
+    }} />
+)
+
+const StartCell = ({ value, ...restProps }) => {
+  const {
+    column
+  } = restProps
+
+  return (
+    <Table.Cell
+      {...restProps}>
+      <span>
+        {
+          value ?
+            [ 1, 2, 3, 4, 5 ].map((currentValue) => (
+              currentValue <= value ? <StarIcon color='primary' key={`star-${column._id}-${currentValue}`} /> :
+                <StarBorderIcon color='primary' key={`star-${column._id}-${currentValue}`} />
+            )) : ''
+        }
+      </span>
+    </Table.Cell>
+  )
+}
+
+const TableCell = (props) => {
+  const { column } = props
+
+  if(column.type === 'star')
+    return <StartCell {...props} />
+
+  return <Table.Cell {...props} />
+}
+
 const TableGroup = props => {
   const {
     classes,
     rows = [],
     columns = [],
     groupingData = [],
-    changeGrouping = () => false,
+    changeGrouping = () => { },
     sorting = false,
     grouping = false,
     sortingData = [],
-    changeSorting = () => false,
+    changeSorting = () => { },
+    withPagination = false,
     withHeader = false,
     withSearch = false,
+    onChangePerPage = () => { },
     withAutocomplete = false,
     searchSuggestions = [],
-    onHandleSearch = () => false,
-    onHandleSelectAutocomplete = () => false
+    onHandleSearch = () => { },
+    groupingDataTemp = null,
+    pagination = {},
+    limitDragable = 1,
+    onChangePage = () => { },
+    onClickRow = () => { },
+    // multiSorting = false,
+    onHandleSelectAutocomplete = () => { }
   } = props
 
-  const [ columnsData, setColumnData ] = useState([])
-  const [ sortingDataFormat, setSortingFormat ] = useState([])
+  const [ columnsData, setColumnData ] = useState(formatColumns(columns))
+  const [ columnsDataExtension, setColumnsDataExtension ] = useState(formatColumnsExtension(columns))
+  const [ sortingDataFormat, setSortingFormat ] = useState(sortingFormat(sortingData))
 
-  const [ groupingStateColumnExtensions, setGroupingStateColumnExtensions ] = useState([])
+  const [ groupingStateColumnExtensions, setGroupingStateColumnExtensions ] = useState(columnsDraggableDisable(columns))
   const [ expandData, setExpandData ] = useState([])
   const inputSearch = useRef(null)
 
@@ -155,6 +229,7 @@ const TableGroup = props => {
     if(columns.length) {
       setColumnData(formatColumns(columns))
       setGroupingStateColumnExtensions(columnsDraggableDisable(columns))
+      setColumnsDataExtension(formatColumnsExtension(columns))
     }
   }, [ columns ])
 
@@ -178,54 +253,54 @@ const TableGroup = props => {
     if(e.keyCode === 13) onHandleSearch(value)
   }
 
-  const onChangeSorting = value => changeSorting(sortingFormatReverse(value))
+  const onChangeSorting = value => {
+    // if(multiSorting)
+    //   if(sortingData.length) {
+    //     const data = [
+    //       ...sortingDataFormat.map(item => item.columnName === value[0].columnName ? value[0] : item),
+    //       ...value
+    //     ]
+    //     changeSorting(sortingFormatReverse(data))
+    //   } else {
+    //   }
+    changeSorting(sortingFormatReverse(value))
+  }
+
+  const onChangeGrouping = value => {
+    if(rows.length)
+      if(value.length <= limitDragable)
+        changeGrouping(value)
+  }
 
   return (
-    <Grid container>
-      <Grid className={classes.containerFullwidth} item xs={12}>
-        <Paper
-          classes={{
-            root: classes.paperRoot
-          }}
-          style={{ position: 'relative' }}>
-          <div className={classes.tableContainer}>
-            {/* search */}
-            {
-              withHeader ? (
-                <div className={classes.containerHeaderTable}>
-                  <div className={classes.containerSearch}>
-                    {withSearch ? withAutocomplete ? (
-                      <Autocomplete
-                        freeSolo
-                        noOptionsText='No hay coincidencias'
-                        onChange={onHandleSelectAutocomplete}
-                        options={searchSuggestions.map(option => option.title)}
-                        popupIcon={<SearchIcon />}
-                        renderInput={params => (
-                          <TextField
-                            {...params}
-                            className={classes.inputSearch}
-                            fullWidth
-                            InputLabelProps={{ shrink: false }}
-                            InputProps={{
-                              ...params.InputProps,
-                              endAdornment: (
-                                <InputAdornment position='end'>
-                                  <SearchIcon className={classes.searchIcon} onClick={() => onHandleSearch(inputSearch.current.value)} />
-                                </InputAdornment>
-                              )
-                            }}
-                            inputRef={inputSearch}
-                            onKeyUp={_handleSearchValidate}
-                            placeholder='Buscar'
-                            variant='outlined' />
-                        )}
-                        style={{ width: 400 }} />
-                    ) : (
+
+    <Paper
+      classes={{
+        root: classes.paperRoot
+      }}
+      style={{ position: 'relative' }}
+      variant='outlined'>
+      <div className={classes.tableContainer}>
+        {/* search */}
+        {
+          withHeader ? (
+            <div className={classes.containerHeaderTable}>
+              <div className={classes.containerSearch}>
+                {withSearch ? withAutocomplete ? (
+                  <Autocomplete
+                    freeSolo
+                    noOptionsText='No hay coincidencias'
+                    onChange={onHandleSelectAutocomplete}
+                    options={searchSuggestions.map(option => option.title)}
+                    popupIcon={<SearchIcon />}
+                    renderInput={params => (
                       <TextField
+                        {...params}
                         className={classes.inputSearch}
+                        fullWidth
                         InputLabelProps={{ shrink: false }}
                         InputProps={{
+                          ...params.InputProps,
                           endAdornment: (
                             <InputAdornment position='end'>
                               <SearchIcon className={classes.searchIcon} onClick={() => onHandleSearch(inputSearch.current.value)} />
@@ -235,63 +310,109 @@ const TableGroup = props => {
                         inputRef={inputSearch}
                         onKeyUp={_handleSearchValidate}
                         placeholder='Buscar'
-                        style={{ width: 400 }}
                         variant='outlined' />
-                    ) : null}
+                    )}
+                    style={{ width: 400 }} />
+                ) : (
+                  <TextField
+                    className={classes.inputSearch}
+                    InputLabelProps={{ shrink: false }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position='end'>
+                          <SearchIcon className={classes.searchIcon} onClick={() => onHandleSearch(inputSearch.current.value)} />
+                        </InputAdornment>
+                      )
+                    }}
+                    inputRef={inputSearch}
+                    onKeyUp={_handleSearchValidate}
+                    placeholder='Buscar'
+                    style={{ width: 400 }}
+                    variant='outlined' />
+                ) : null}
 
-                  </div>
-                </div>
-              ) : null
-            }
+              </div>
+            </div>
+          ) : null
+        }
 
-            <GridTable
-              columns={columnsData}
-              getRowId={getRowId}
-              rows={rows}>
+        <GridTable
+          columns={columnsData}
+          getRowId={getRowId}
+          rows={rows}>
 
-              {/* sorting */}
-              <SortingState
-                onSortingChange={onChangeSorting}
-                sorting={sortingDataFormat} />
+          {/* sorting */}
+          <SortingState
+            onSortingChange={onChangeSorting}
+            sorting={sortingDataFormat} />
 
-              {/* grouping */}
-              {
-                grouping &&
-                  <DragDropProvider />
-              }
-              <GroupingState
-                // expandedGroups={[ 'Confirmed' ]}
-                columnExtensions={groupingStateColumnExtensions}
-                grouping={groupingData}
-                onExpandedGroupsChange={setExpandedGroups}
-                onGroupingChange={changeGrouping} />
-              <CustomGrouping
-                expandedGroups={expandData}
-                getChildGroups={getChildGroups}
-                grouping={null} />
+          {/* grouping */}
+          {
+            grouping && <DragDropProvider />
+          }
+          <GroupingState
+            // expandedGroups={[ 'Confirmed' ]}
+            columnExtensions={groupingStateColumnExtensions}
+            grouping={groupingData}
+            onExpandedGroupsChange={setExpandedGroups}
+            onGroupingChange={onChangeGrouping} />
+          <CustomGrouping
+            expandedGroups={expandData}
+            getChildGroups={getChildGroups}
+            grouping={groupingDataTemp} />
+          <IntegratedSorting />
 
-              <Table />
+          <Table
+            cellComponent={TableCell}
+            columnExtensions={columnsDataExtension}
+            rowComponent={row => TableRow(row, onClickRow)} />
 
-              <TableHeaderRow messages={{ sortingHint: 'Ordenar' }} showSortingControls={sorting} />
+          <TableHeaderRow
+            messages={{ sortingHint: 'Ordenar' }}
+            showSortingControls={/* groupingData.length ? false : */ sorting} />
 
-              <TableGroupRow
-                rowComponent={({ children }) =>(
-                  <TableHeaderRow.Row className={classes.gridTableHeaderGroup}>{children}</TableHeaderRow.Row>
-                )} />
+          <TableGroupRow
+            rowComponent={({ children }) => (
+              <TableHeaderRow.Row className={classes.gridTableHeaderGroup}>{children}</TableHeaderRow.Row>
+            )} />
 
-              <Toolbar />
+          <Toolbar />
 
-              <GroupingPanel
-                emptyMessageComponent={() => (
-                  <div className={classes.gridTableGroupingEmpty}>Arrastra aquí una columna para agrupar tus registros.</div>
-                )}
-                showGroupingControls />
+          <GroupingPanel
+            emptyMessageComponent={() => (
+              <div className={classes.gridTableGroupingEmpty}>Arrastra aquí una columna para agrupar tus registros.</div>
+            )}
+            itemComponent={ev => {
+              const customSorting = true
+              // const customSorting = (ev.item && ev.item.column && groupingData.length) ?
+              //   Boolean(ev.item.column.name === groupingData[0].columnName) : true
 
-            </GridTable>
-          </div>
-        </Paper>
-      </Grid>
-    </Grid>
+              return <GroupingPanel.Item {...ev} className={classes.gridTableChip} sortingEnabled={customSorting} />
+            }}
+            showGroupingControls
+            showSortingControls />
+
+        </GridTable>
+        {
+          withPagination ?
+            <div className={classes.contetPagination}>
+              <TablePagination
+                backIconButtonText='Página anterior'
+                component='div'
+                labelRowsPerPage='Mostrar'
+                nextIconButtonText='Página siguiente'
+                onChangePage={onChangePage}
+                onChangeRowsPerPage={onChangePerPage}
+                page={pagination.page}
+                rowsPerPage={pagination.perPage}
+                rowsPerPageOptions={[ 10, 25, 100 ]}
+                totalItems={pagination.totalItems}
+                totalPages={pagination.totalPages} />
+            </div> : null
+        }
+      </div>
+    </Paper>
+
   )
 }
 
@@ -312,8 +433,13 @@ TableGroup.propTypes = {
   groupingData: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string
   })),
+  limitDragable             : PropTypes.number,
+  onChangePage              : PropTypes.func,
+  onChangePerPage           : PropTypes.func,
+  onClickRow                : PropTypes.func,
   onHandleSearch            : PropTypes.func,
   onHandleSelectAutocomplete: PropTypes.func,
+  pagination                : PropTypes.object,
   rows                      : PropTypes.arrayOf(
     PropTypes.shape({
       _id: PropTypes.string.isRequired
@@ -327,6 +453,7 @@ TableGroup.propTypes = {
   })),
   withAutocomplete: PropTypes.bool,
   withHeader      : PropTypes.bool,
+  withPagination  : PropTypes.bool,
   withSearch      : PropTypes.bool
 }
 
