41d962919dca8d6049ea59c8d60edd956d118292
LuisSullca <luis.sullca.h@uni.pe>
2020-11-13 13:12:50 -0500

    [update] se agrego nuevo prop a componente avataruser

--- docs/src/containers/views/avataruser/AvatarUser.js
+++ docs/src/containers/views/avataruser/AvatarUser.js
@@ -2,13 +2,27 @@ import React from 'react'
 import { AvatarUser } from '@krowdy-ui/views'
 
 export default function () {
+  const _handleDelete = () => {
+
+  }
+
   return (
     <div>
-      <AvatarUser user={{
-        firstName: 'Xavi',
-        lastName : 'Gonzalez',
-        photo    : ''
-      }} />
+      <AvatarUser
+        size='small'
+        user={{
+          firstName: 'Xavi',
+          lastName : 'Gonzalez',
+          photo    : ''
+        }} />
+      <AvatarUser
+        onDelete={_handleDelete}
+        size='small'
+        user={{
+          firstName: 'Xavi',
+          lastName : 'Gonzalez',
+          photo    : ''
+        }} />
     </div>
   )
 }
