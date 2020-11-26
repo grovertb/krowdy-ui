06eb23812c1fe496da1d2340ff4d745ac767ddae
Anthony Bryan Gavilan Vinces <abgv9221@gmail.com>
2020-11-26 16:30:54 -0500

    [ ADD ] design

--- docs/src/containers/views/listinfo/ListInfo.js
+++ docs/src/containers/views/listinfo/ListInfo.js
@@ -1,11 +1,48 @@
 import React from 'react'
-import { Grid } from '@krowdy-ui/core'
+import { Button, Grid } from '@krowdy-ui/core'
+import {
+  Add as AddIcon
+} from '@material-ui/icons'
 import { ListInfo } from '@krowdy-ui/views'
 
+// const useStyles = makeStyles((theme) => ({
+//   formControl: {
+//     margin  : theme.spacing(1),
+//     minWidth: 200
+//   }
+// }))
+
+const list = [
+  {
+    _id  : '1',
+    title: 'No aplica'
+  },
+  {
+    _id     : '2',
+    selected: true,
+    subtitle: 'Alicorp',
+    title   : 'Diseñador UX'
+  },
+  {
+    _id     : '3',
+    subtitle: 'Coca - Cola',
+    title   : 'Diseñador UI'
+  }
+]
+
 export default function () {
+  // const classes = useStyles()
+
+  const _handleChange = (id) => () => {
+    console.log(id)
+  }
+
   return (
     <Grid container>
-      <ListInfo />
+      <ListInfo
+        header={<Button color='primary' startIcon={<AddIcon />}>Agregar puesto</Button>}
+        list={list}
+        onChange={_handleChange} />
     </Grid>
   )
 }
--- packages/krowdy-ui-views/src/ListInfo/ListInfo.js
+++ packages/krowdy-ui-views/src/ListInfo/ListInfo.js
@@ -9,29 +9,91 @@ import {
   withStyles
 } from '@krowdy-ui/core'
 import {
-  Image as ImageIcon,
-  Done as DoneIcon
+  Done as DoneIcon,
+  Domain as DomainIcon
 } from '@material-ui/icons'
+import { Divider } from '../../../krowdy-ui/src/index'
 
-const styles = (/* theme */) => ({
+const styles = (theme) => ({
+  avatar: {
+    backgroundColor: theme.palette.secondary[10],
+    color          : theme.palette.secondary[200]
+  },
+  button: {
+    '&:hover': {
+      backgroundColor: theme.palette.primary[10]
+    }
+  },
   container: {
-    width: 298
+    padding: theme.spacing(1.5),
+    width  : 298
+  },
+  gutters: {
+    paddingLeft : theme.spacing(1),
+    paddingRight: theme.spacing(1)
+  },
+  header: {
+    alignItems    : 'center',
+    display       : 'flex',
+    justifyContent: 'center',
+    margin        : theme.spacing(1.625)
+  },
+  image: {
+    color: theme.palette.secondary[200]
+  },
+  secondary: {
+    fontSize: 12
   }
 })
 
-const ListInfo = ({ classes }) => (
+const ListInfo = ({ classes, header, list, onChange }) => (
   <Card className={classes.container}>
-    <List>
-      <ListItem button>
-        <ListItemAvatar>
-          <Avatar>
-            <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary='Photos' secondary='Jan 9, 2014' />
-        <DoneIcon color='primary' />
-      </ListItem>
-    </List>
+    {
+      header ? (
+        <>
+          <div className={classes.header}>
+            { header }
+          </div>
+          <Divider />
+        </>
+      ) : null
+    }
+
+    <div>
+      {
+        list.length ? (
+          <List>
+            {list.map(({ icon: Icon, src, title, subtitle, selected, _id }) => (
+              <ListItem
+                button
+                classes={{
+                  button : classes.button,
+                  gutters: classes.gutters
+                }}
+                key={_id}
+                onClick={onChange ? onChange(_id) : undefined}>
+                <ListItemAvatar>
+                  <Avatar
+                    alt={title}
+                    className={classes.avatar}
+                    src={src}
+                    variant='rounded' >
+                    {Icon ? <Icon className={classes.image} /> : <DomainIcon className={classes.image} />}
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  classes={{
+                    secondary: classes.secondary
+                  }}
+                  primary={title}
+                  secondary={subtitle} />
+                {selected ? <DoneIcon color='primary' /> : null}
+              </ListItem>
+            ))}
+          </List>
+        ) : null
+      }
+    </div>
   </Card>
 )
 
