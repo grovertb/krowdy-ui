348fc08f1d90b9c065b6caf7a1cc21531285f64c
maytesi <maytesamaniegoi@gmail.com>
2021-05-10 12:58:10 -0500

    [UPDATE] comments

--- docs/src/containers/views/rangeschedule/RangeSchedule.js
+++ docs/src/containers/views/rangeschedule/RangeSchedule.js
@@ -2,14 +2,13 @@ import React, { useMemo } from 'react'
 import { Grid } from '@material-ui/core'
 import { RangeSchedule } from '@krowdy-ui/views'
 
-const today = new Date(new Date().setHours(24)).getDate()
 const month = new Date().getMonth()
 const year = new Date().getFullYear()
 
 export default function () {
   const initialRange = useMemo(()=>{
-    const minDate = `${month + 1}/${today}/${year}`
-    const maxDate = `${month === 12 ? 1 : month + 2}/${today}/${year}`
+    const minDate = `${month === 12 ? 1 : month + 1}/15/${year}`
+    const maxDate = `${month === 11 ? 1 : month === 12 ? 2 : month + 2}/15/${year}`
 
     return { maxDate, minDate }
   }, [])
