1edeb9b023e347e1d5ac02cc6c11fab1488a2589
LuisSullca <luis.sullca.h@uni.pe>
2021-05-31 11:31:55 -0500

    [update] se corrigio material-icons

---
+++ packages/krowdy-ui-icons/README.md
@@ -0,0 +1,19 @@
+# @krowdy-ui/styles
+
+You can leverage our styling solution, even if you are not using our components.
+
+## Installation
+
+Install the package in your project directory with:
+
+```sh
+// with npm
+npm install @krowdy-ui/styles
+
+// with yarn
+yarn add @krowdy-ui/styles
+```
+
+## Documentation
+
+[The documentation](https://ui.krowdy.com/)
---
+++ packages/krowdy-ui-icons/src/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable import/export */
+export * from '@material-ui/icons'
+
+export { default as AccessTimeThin } from './AccessTimeThin'
+export * from './AccessTimeThin'
+
+export { default as AddCircleOutlinedThin } from './AddCircleOutlinedThin'
+export * from './AddCircleOutlinedThin'
+
+export { default as ComputerThin } from './ComputerThin'
+export * from './ComputerThin'
+
+export { default as DressHookThin } from './DressHookThin'
+export * from './DressHookThin'
+
+export { default as DriveEtaOutlinedThin } from './DriveEtaOutlinedThin'
+export * from './DriveEtaOutlinedThin'
+
+export { default as EmojiPeopleOutlinedThin } from './EmojiPeopleOutlinedThin'
+export * from './EmojiPeopleOutlinedThin'
+
+export { default as LanguageThin } from './LanguageThin'
+export * from './LanguageThin'
+
+export { default as PayrollThin } from './PayrollThin'
+export * from './PayrollThin'
+
+export { default as PetsOutlinedThin } from './PetsOutlinedThin'
+export * from './PetsOutlinedThin'
+
