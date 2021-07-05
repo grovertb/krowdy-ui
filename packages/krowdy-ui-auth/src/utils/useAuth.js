8766fcde19d6ae3bbaa5203e716f57c6d7245f23
mayte <maytesamaniegoi@gmail.com>
2021-07-05 18:20:05 -0500

    [REMOVE] comentarios

--- packages/krowdy-ui-auth/src/utils/useAuth.js
+++ packages/krowdy-ui-auth/src/utils/useAuth.js
@@ -4,23 +4,3 @@ import LoginContext from '../AuthContext/LoginContext'
 const useAuth = () => useContext(LoginContext)
 
 export default useAuth
-
-// export const useVerifyLogin = () => {
-//   const verifyLogin = async (onFinish) => {
-//     try {
-//       const { localStorage } = window
-//       const accessToken = localStorage.getItem('accessToken')
-//       if(!accessToken) {
-//         onFinish(false)
-//       } else {
-//         const { data } = (await accountAPI.get(accountsApiUrl + '/api/authenticate')) || {}
-
-//         onFinish(data?.success)
-//       }
-//     } catch (error) {
-//       onFinish(false)
-//     }
-//   }
-
-//   return [ verifyLogin ]
-// }
