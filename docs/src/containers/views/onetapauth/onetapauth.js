e49dd0ccfdd8bfc6c7fedf06d7126bcf6247ad89
Jesus <jesusk01@gmail.com>
2021-07-13 18:56:52 -0500

    [UPDATE] docs

--- docs/src/containers/views/onetapauth/onetapauth.js
+++ docs/src/containers/views/onetapauth/onetapauth.js
@@ -8,18 +8,9 @@ export default function () {
       AuthContextProps={{
         baseUrl: 'http://localhost:9876',
         social : {
-          google: {
-            clientId: '612106570053-ke4agjifh4j78e4od0i5l3duh85u6tl7.apps.googleusercontent.com'
-          },
-          linkedin: {
-            clientId   : '77ryo6ilm04r9z',
-            redirectUri: 'http://localhost:3000/views/onetapauth',
-            scope      : 'r_emailaddress'
-          },
-          microsoft: {
-            clientId   : '8a78aebe-f6c7-4371-9974-c72d6cd019bc',
-            redirectUri: 'http://localhost:3000'
-          }
+          google   : {},
+          linkedin : {},
+          microsoft: {}
         },
         urlLogin: 'http://localhost:3001'
       }}>
